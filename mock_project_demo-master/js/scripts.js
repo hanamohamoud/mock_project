a50431e47ba5b25cd70ee9611e2f7aa89b5bc6bc
Hana Mohamoud <hanamohamoud@hotmail.com>
2023-04-19 20:32:13 +0100

    first commit

---
+++ mock_project_demo-master/js/scripts.js
@@ -0,0 +1,12 @@
+// "listens" for when the window is scrolled on. When a scroll happens, "scrollFunction" is executed
+$(window).scroll(scrollFunction);
+
+// When the user scrolls down 20px from the top of the document, show the button
+function scrollFunction() {
+ 
+};
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+ 
+}
